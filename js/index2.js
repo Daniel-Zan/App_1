ec0745009e414e183c8d4e0fe2c5db93d5a70345
Sala D8 <CSalaD8@uniboyaca.edu.co>
2023-04-01 11:59:30 -0500

    Ahora si que si

--- js/index2.js
+++ js/index2.js
@@ -1,10 +1,10 @@
-$(document).ready(function() {
-    $('#verificar').on('click', function() {
-        const valor = $('#valor').val();
-        if (isNaN(valor)) {
-            $('#resultado').text('El valor ingresado es un texto');
-        } else {
-            $('#resultado').text('El valor ingresado es un número');
-        }
-    });
+$(document).ready(function () {
+  $("#verificar").on("click", function () {
+    const valor = $("#valor").val();
+    if (isNaN(valor)) {
+      $("#resultado").text("El valor ingresado es un texto");
+    } else {
+      $("#resultado").text("El valor ingresado es un número");
+    }
+  });
 });
--- js/index7.js
+++ js/index7.js
@@ -1,14 +1,14 @@
-$(document).ready(function(){
-	$("#form").submit(function(event){
-		event.preventDefault();
-		var num1 = $("#num1").val();
-		var num2 = $("#num2").val();
-		if (num1 % num2 == 0) {
-			$("#result").text(num1 + " es divisible por " + num2);
-		} else if (num2 % num1 == 0) {
-			$("#result").text(num2 + " es divisible por " + num1);
-		} else {
-			$("#result").text("Los números no son divisibles entre sí");
-		}
-	});
+$(document).ready(function () {
+  $("#form").submit(function (event) {
+    event.preventDefault();
+    var num1 = $("#num1").val();
+    var num2 = $("#num2").val();
+    if (num1 % num2 == 0) {
+      $("#result").text(num1 + " es divisible por " + num2);
+    } else if (num2 % num1 == 0) {
+      $("#result").text(num2 + " es divisible por " + num1);
+    } else {
+      $("#result").text("Los números no son divisibles entre sí");
+    }
+  });
 });
