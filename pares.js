a6e66b8c4ce9bb96d72557ec422307c3199f723a
Daniel-Zan <dansanmartinez@uniboyaca.edu.co>
2023-03-31 22:24:07 -0500

    Avance 5

---
+++ pares.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('form').submit(function(e) {
+        e.preventDefault();
+        var num = $('#numberInput').val();
+
+   
+        if (num % 1 !== 0 || num < 0) {
+            $('#result').text('Ingresa un número entero positivo válido');
+            return;
+        }
+
+
+        if (num % 2 === 0) {
+            $('#result').text(num + ' es un número par');
+        } else {
+            $('#result').text(num + ' es un número impar');
+        }
+    });
+});
