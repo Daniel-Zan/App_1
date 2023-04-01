4aa2c867f542e5cf9520d3da05f2cc2580891b9d
Daniel-Zan <dansanmartinez@uniboyaca.edu.co>
2023-04-01 09:03:11 -0500

    7 y 8 hechas, falta maquetacion y color

---
+++ par.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+    const button = document.getElementById('calcular');
+    const numeros = document.getElementById('numeros');
+    const resultado = document.getElementById('resultado');
+  
+    button.addEventListener('click', function() {
+  
+      const lista = numeros.value.split(",");
+      let pares = "";
+      let impares = "";
+  
+      for (let i = 0; i < lista.length; i++) {
+        let num = parseInt(lista[i]);
+        if (num % 2 === 0) {
+          pares += num + ", ";
+        } else {
+          impares += num + ", ";
+        }
+      }
+  
+      if (pares === "") {
+        resultado.innerHTML = "No hay números pares.";
+      } else {
+        resultado.innerHTML = "Pares: " + pares.slice(0, -2) + ".";
+      }
+  
+      if (impares === "") {
+        resultado.innerHTML += "<br>No hay números impares.";
+      } else {
+        resultado.innerHTML += "<br>Impares: " + impares.slice(0, -2) + ".";
+      }
+  
+    });
+  
+  });
+  
