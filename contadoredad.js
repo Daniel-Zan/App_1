b04750b3752ed19d63cd8ba7c6ca614eaf9c39ac
Sala D8 <CSalaD8@uniboyaca.edu.co>
2023-03-31 19:22:24 -0500

    Ejercicio 3, terminar la maquetación pendiente

---
+++ contadoredad.js
@@ -0,0 +1,9 @@
+function verificarEdad() {
+    const nombre = document.getElementById("nombre").value;
+    const edad = document.getElementById("edad").value;
+    if (edad >= 18) {
+      document.getElementById("resultado").innerHTML = nombre + " es mayor de edad.";
+    } else {
+      document.getElementById("resultado").innerHTML = nombre + " no es mayor de edad.";
+    }
+  }
